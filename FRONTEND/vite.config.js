c5e3d233a770b2f2aa60cab13d1e8a006569c5d6
Omer Kh <omer8410243@gmail.com>
2026-09-05 12:23:49 +0100

    05/09/2026 - Finalisation de la Configuration du Front-end pour être déployé sur Vercel

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://54.155.99.199',
         changeOrigin: true,
       },
     },
